9555f09d52db3828008451da17e548ca2aad16f6
NhienTran133 <hiennhien130305@gmail.com>
2026-03-18 16:00:27 +0700

    Nhiên

---
+++ app/Http/Controllers/NameController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class NameController extends Controller{
+    function HienThiTen(){
+    return 'Trần Hiển Nhiên';
+  }
+    }
+
+?>
