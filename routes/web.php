b1b3f4f21c6a6b4b78a2d12c9008267b476c4c77
030239230142-dpn <030239230142@st.buh.edu.vn>
2026-03-18 16:46:14 +0700

    Lưu tạm thay đổi trước khi pull

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dangphuongnghi', function () {
+    return "Đặng Phương Nghi";
+});
